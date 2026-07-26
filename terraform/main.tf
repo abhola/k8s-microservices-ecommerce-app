300c8283957ea32c8ea06770ce05937d99015eb8
abhola <ankurhcl01@gmail.com>
2026-07-27 00:02:56 +0530

    files chnaged

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@
 # =============================================================================
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws" 
   version = "~> 5.0"
 
   name = "${var.cluster_name}-vpc"
